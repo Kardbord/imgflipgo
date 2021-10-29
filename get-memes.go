8397eb0801566a40b3548b11ded0786de07c7e64
Tanner Kvarfordt <tanner.kvarfordt@gmail.com>
2021-10-29 14:06:11 -0600

    Implemented GetMemes

--- get-memes.go
+++ get-memes.go
@@ -1,1 +1,63 @@
 package imgflipgo
+
+import (
+	"encoding/json"
+	"errors"
+	"io"
+	"net/http"
+)
+
+const GetMemesEndpoint string = "https://api.imgflip.com/get_memes"
+
+type Meme struct {
+	ID     string `json:"id,omitempty"`
+	Name   string `json:"name,omitempty"`
+	URL    string `json:"url,omitempty"`
+	Width  uint   `json:"width,omitempty"`
+	Height uint   `json:"height,omitempty"`
+
+	// BoxCount describes the number of text boxes the Meme uses.
+	BoxCount uint `json:"box_count,omitempty"`
+}
+
+type MemesResponse struct {
+	Success bool `json:"success,omitempty"`
+	Data    struct {
+		Memes []Meme `json:"memes,omitempty"`
+	} `json:"data,omitempty"`
+}
+
+func GetMemesResponse() (*MemesResponse, error) {
+	resp, err := http.Get(GetMemesEndpoint)
+	if err != nil {
+		return nil, err
+	}
+	if resp == nil {
+		return nil, errors.New("nil response received")
+	}
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	memesResp := MemesResponse{}
+	err = json.Unmarshal(body, &memesResp)
+	if err != nil {
+		return nil, err
+	}
+
+	return &memesResp, err
+}
+
+func GetMemes() ([]Meme, error) {
+	memesResp, err := GetMemesResponse()
+	if err != nil {
+		return nil, err
+	}
+	if memesResp == nil {
+		return nil, errors.New("nil response received")
+	}
+	return memesResp.Data.Memes, nil
+}
---
+++ get-memes_test.go
@@ -0,0 +1,32 @@
+package imgflipgo_test
+
+import (
+	"testing"
+
+	"github.com/TannerKvarfordt/imgflipgo"
+)
+
+func TestGetMemesResponse(t *testing.T) {
+	memesResp, err := imgflipgo.GetMemesResponse()
+	if err != nil {
+		t.Fatal(err)
+	}
+	if !memesResp.Success {
+		t.Fatal("GET was not successful")
+	}
+	if len(memesResp.Data.Memes) < 1 {
+		t.Fatal("No memes were retrieved despite successful request")
+	}
+	t.Logf("Retrieved %d memes", len(memesResp.Data.Memes))
+}
+
+func TestGetMemes(t *testing.T) {
+	memes, err := imgflipgo.GetMemes()
+	if err != nil {
+		t.Fatal(err)
+	}
+	if len(memes) < 1 {
+		t.Fatal("No memes were retrieved despite successful request")
+	}
+	t.Logf("Retrieved %d memes", len(memes))
+}

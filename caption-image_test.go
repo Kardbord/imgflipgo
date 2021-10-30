c3f7e3942f941303ec389395c6f8301431a94ffa
Tanner Kvarfordt <tanner.kvarfordt@gmail.com>
2021-10-30 15:44:23 -0600

    Additional CaptionImage unit tests

--- caption-image_test.go
+++ caption-image_test.go
@@ -1,12 +1,34 @@
 package imgflipgo_test
 
 import (
+	"math/rand"
+	"os"
 	"testing"
 
 	"github.com/TannerKvarfordt/imgflipgo"
+	"github.com/joho/godotenv"
 )
 
-const testTemplateID = "181913649"
+func TestMain(m *testing.M) {
+	setup()
+	os.Exit(m.Run())
+}
+
+const (
+	ImgflipAPIUsernameEnv = "IMGFLIP_API_USERNAME"
+	ImgflipAPIPasswordEnv = "IMGFLIP_API_PASSWORD"
+)
+
+var (
+	ImgflipAPIUsername string
+	ImgflipAPIPassword string
+)
+
+func setup() {
+	godotenv.Load()
+	ImgflipAPIUsername, _ = os.LookupEnv(ImgflipAPIUsernameEnv)
+	ImgflipAPIPassword, _ = os.LookupEnv(ImgflipAPIPasswordEnv)
+}
 
 func expectNilError(t *testing.T, err error) {
 	if err != nil {
@@ -36,9 +58,19 @@ func expectUnsuccessfulNoErr(t *testing.T, resp *imgflipgo.CaptionResponse, err 
 	expectNilError(t, err)
 	expectNonNilResponse(t, resp)
 	if resp.Success {
-		t.Fatalf("Expected request to fail")
+		t.Fatal("Expected request to fail")
 	}
 }
+
+func expectSuccess(t *testing.T, resp *imgflipgo.CaptionResponse, err error) {
+	expectNilError(t, err)
+	expectNonNilResponse(t, resp)
+	if !resp.Success {
+		t.Fatalf("Expected request to succeed, err=%s", resp.ErrorMsg)
+	}
+}
+
+const testTemplateID = "181913649"
 
 func TestCaptionImageNilRequest(t *testing.T) {
 	resp, err := imgflipgo.CaptionImage(nil)
@@ -85,4 +117,108 @@ func TestCaptionImageInvalidAuth(t *testing.T) {
 		BottomText: "BOTTOM TEXT",
 	})
 	expectUnsuccessfulNoErr(t, resp, err)
+}
+
+func TestCaptionImageTopTextOnly(t *testing.T) {
+	resp, err := imgflipgo.CaptionImage(&imgflipgo.CaptionRequest{
+		TemplateID: testTemplateID,
+		Username:   ImgflipAPIUsername,
+		Password:   ImgflipAPIPassword,
+		TopText:    "Top Text",
+	})
+	expectSuccess(t, resp, err)
+	t.Logf("See test image at %s", resp.Data.URL)
+}
+
+func TestCaptionImageBottomTextOnly(t *testing.T) {
+	resp, err := imgflipgo.CaptionImage(&imgflipgo.CaptionRequest{
+		TemplateID: testTemplateID,
+		Username:   ImgflipAPIUsername,
+		Password:   ImgflipAPIPassword,
+		BottomText: "Bottom Text",
+	})
+	expectSuccess(t, resp, err)
+	t.Logf("See test image at %s", resp.Data.URL)
+}
+
+func TestCaptionImageTopAndBottomText(t *testing.T) {
+	resp, err := imgflipgo.CaptionImage(&imgflipgo.CaptionRequest{
+		TemplateID: testTemplateID,
+		Username:   ImgflipAPIUsername,
+		Password:   ImgflipAPIPassword,
+		TopText:    "Top Text",
+		BottomText: "Bottom Text",
+	})
+	expectSuccess(t, resp, err)
+	t.Logf("See test image at %s", resp.Data.URL)
+}
+
+func TestCaptionImageArial(t *testing.T) {
+	resp, err := imgflipgo.CaptionImage(&imgflipgo.CaptionRequest{
+		TemplateID: testTemplateID,
+		Username:   ImgflipAPIUsername,
+		Password:   ImgflipAPIPassword,
+		TopText:    "Top Text",
+		BottomText: "Bottom Text",
+		Font:       imgflipgo.FontArial,
+	})
+	expectSuccess(t, resp, err)
+	t.Logf("See test image at %s", resp.Data.URL)
+}
+
+func TestCaptionImageImpact(t *testing.T) {
+	resp, err := imgflipgo.CaptionImage(&imgflipgo.CaptionRequest{
+		TemplateID: testTemplateID,
+		Username:   ImgflipAPIUsername,
+		Password:   ImgflipAPIPassword,
+		TopText:    "Top Text",
+		BottomText: "Bottom Text",
+		Font:       imgflipgo.FontImpact,
+	})
+	expectSuccess(t, resp, err)
+	t.Logf("See test image at %s", resp.Data.URL)
+}
+
+func TestCaptionImageMaxFontSize(t *testing.T) {
+	resp, err := imgflipgo.CaptionImage(&imgflipgo.CaptionRequest{
+		TemplateID:    testTemplateID,
+		Username:      ImgflipAPIUsername,
+		Password:      ImgflipAPIPassword,
+		TopText:       "Top Text",
+		BottomText:    "Bottom Text",
+		MaxFontSizePx: uint(rand.Intn(int(imgflipgo.DefaultMaxFontSizePx))),
+	})
+	expectSuccess(t, resp, err)
+	t.Logf("See test image at %s", resp.Data.URL)
+}
+
+func TestCaptionImageTopTextbox(t *testing.T) {
+	resp, err := imgflipgo.CaptionImage(&imgflipgo.CaptionRequest{
+		TemplateID: testTemplateID,
+		Username:   ImgflipAPIUsername,
+		Password:   ImgflipAPIPassword,
+		TextBoxes: []imgflipgo.TextBox{
+			{
+				Text: "Top Text",
+			},
+		},
+	})
+	expectSuccess(t, resp, err)
+	t.Logf("See test image at %s", resp.Data.URL)
+}
+
+func TestCaptionImageBottomTextbox(t *testing.T) {
+	resp, err := imgflipgo.CaptionImage(&imgflipgo.CaptionRequest{
+		TemplateID: testTemplateID,
+		Username:   ImgflipAPIUsername,
+		Password:   ImgflipAPIPassword,
+		TextBoxes: []imgflipgo.TextBox{
+			{},
+			{
+				Text: "Bottom Text",
+			},
+		},
+	})
+	expectSuccess(t, resp, err)
+	t.Logf("See test image at %s", resp.Data.URL)
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/TannerKvarfordt/imgflipgo
 
 go 1.17
 
-require github.com/gorilla/schema v1.2.0
+require (
+	github.com/gorilla/schema v1.2.0
+	github.com/joho/godotenv v1.4.0
+)
